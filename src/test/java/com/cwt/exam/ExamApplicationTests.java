656030ce5958aa9c62e491f73340b3d9b3c81b53
COLLABERA <sieghart415@gmail.com>
2022-07-20 14:07:53 +0800

    second commit

--- src/test/java/com/cwt/exam/ExamApplicationTests.java
+++ src/test/java/com/cwt/exam/ExamApplicationTests.java
@@ -3,11 +3,16 @@ package com.cwt.exam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class ExamApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int num = 1;
+
+		assertTrue(5 > num);
 	}
 
 }
